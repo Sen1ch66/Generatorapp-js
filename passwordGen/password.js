d320727b79ba4e947230cf1566c0d84fff827e30
Sen1ch66 <arsenzahovajko@gmail.com>
2024-05-20 01:29:15 +0300

    Готово

--- passwordGen/password.js
+++ passwordGen/password.js
@@ -4,24 +4,26 @@ genBtn.addEventListener("click", gen);
 function gen() {
     try {
         const inputVal = document.querySelector("#password").value;
-        const highReg = document.querySelector("#highReg").value;
-        const LowReg = document.querySelector("#LowReg").value;
-        const nums = document.querySelector("#nums").value;
-        console.log(Boolean(LowReg))
-        // if (inputVal < 5 || inputVal > 20) {
-        //     alert("Недоступна Довжина")
-        // } else if (!highReg && !LowReg && !nums) {
-        //     alert("Виберіть регістр")
-        // } else {
-        //     fetch(`https://www.random.org/strings/?num=1&len=${inputVal}&digits=${nums}&upperalpha=${highReg}&loweralpha=${LowReg}&unique=on&format=plain&rnd=new`)
-        //         .then(response => response.text())
-        //         .then(response => {
-        //             const showResult = document.createElement("p")
-        //             showResult.textContent = 'Пароль: ' + response;
-        //             showResult.classList.add("fetchPassword")
-        //             mainSec.appendChild(showResult);
-        //         });
-        // }
+        let highReg = document.querySelector("#highReg").checked;
+        let LowReg = document.querySelector("#LowReg").checked;
+        let nums = document.querySelector("#nums").checked;
+        nums ? nums = 'on' : nums = 'off';
+        LowReg ? LowReg = 'on' : LowReg = 'off';
+        highReg ? highReg = 'on' : highReg = 'off';
+        if (inputVal < 5 || inputVal > 20) {
+            alert("Недоступна Довжина")
+        } else if (!highReg && !LowReg && !nums) {
+            alert("Виберіть регістр")
+        } else {
+            fetch(`https://www.random.org/strings/?num=1&len=${inputVal}&digits=${nums}&upperalpha=${highReg}&loweralpha=${LowReg}&unique=on&format=plain&rnd=new`)
+                .then(response => response.text())
+                .then(response => {
+                    const showResult = document.createElement("p")
+                    showResult.textContent = 'Пароль: ' + response;
+                    showResult.classList.add("fetchPassword")
+                    mainSec.appendChild(showResult);
+                });
+        }
     }
     catch(e){
         console.log(e)
